f2636e07c691a3f549725354ea328f55de3c3319
jake <jcgroberts@gmail.com>
2023-02-26 19:31:48 +0000

    hi justin <3

--- docs/compare.js
+++ docs/compare.js
@@ -11,6 +11,14 @@ class ComparePositions {
             left_ankle: {A: 23, B: 25, C: 27}, 
             right_ankle: {A: 24, B: 26, C: 28}
         }
+        this.additionalJoints = {
+            left_elbow: {A: 12, B: 11, C: 13},
+            right_elbow:{A: 11, B: 12, C: 14},
+            left_knee: {A: 11, B: 23, C: 25}, 
+            right_knee: {A: 12, B: 24, C: 26}, 
+            left_shoulder: {A: 11, B: 23, C: 25}, 
+            right_shoulder: {A: 12, B: 24, C: 26}
+        }
         this.angleDiff = {
             left_wrist: 0,
             right_wrist: 0,
@@ -19,7 +27,9 @@ class ComparePositions {
             left_knee: 0,
             right_knee: 0,
             left_ankle: 0,
-            right_ankle: 0
+            right_ankle: 0,
+            left_shoulder: 0,
+            right_shoulder: 0
         }
     }
 
@@ -55,10 +65,33 @@ class ComparePositions {
         return base.concat(user);
     }
 
+    //adds left and right shoulder, changes elbows+knees
+    additionalPoints(baseFrame, userFrame){
+        for (let key in this.additionalJoints) {
+            let points = this.getPoints(baseFrame, userFrame, key);
+            for (let point in points) {
+                if (point.score < this.minScore) {
+                    return this.angleDiff;
+                }
+            }
+            let baseAngle = this.getAngle(points[0], points[1], points[2]);
+            let userAngle = this.getAngle(points[0], points[1], points[2]);
+            if (key == "left_shoulder" || key == "right_shoulder"){
+                this.angleDiff[key] = baseAngle - userAngle;
+            }
+            else{
+                this.angleDiff[key] /= 2;
+                this.angleDiff[key] += (baseAngle - userAngle)/2;
+            }
+        }
+        return this.angleDiff;
+    }
+
     next(baseFrame, userFrame) {
         for (let key in this.joints) {
             let points = this.getPoints(baseFrame, userFrame, key);
             for (let point in points) {
+                //NOT SURE ABT THIS
                 if (point.score < this.minScore) {
                     return this.angleDiff;
                 }
