a9325036c845acfd5db9853fdb30b7199038651c
Justin Yeung <la19074@bristol.ac.uk>
2023-02-27 07:28:29 +0000

    added scoreboard backend

--- docs/ScoreBoard.js
+++ docs/ScoreBoard.js
@@ -1,17 +1,30 @@
 class ScoreBoard {
     addScore(fileName, score) {
-        let scoreBoard = localStorage.getItem('scoreBoard');
-        if (scoreBoard == undefined) {
-            scoreBoard = [];
-        }
-        scoreBoard.unshift([fileName, score]);
-        while(scoreBoard > 10) {
+        let scoreBoard = this.getScoreBoard();
+        scoreBoard.unshift(String(score));
+        scoreBoard.unshift(fileName);
+        while(scoreBoard.length > 20) {
+            scoreBoard.pop();
             scoreBoard.pop();
         }
-        localStorage.setItem('scoreBoard', scoreBoard);
+        for (let i = 0; i < 20; i += 2) {
+            if (scoreBoard[i] != null && scoreBoard[i + 1] != null) {
+                localStorage.setItem('fileName' + i / 2, scoreBoard[i]);
+                localStorage.setItem('score' + i / 2, scoreBoard[i + 1]);
+            }
+        }
     }
 
-    getScore() {
-        let scoreBoard = localStorage.getItem('scoreBoard');
+    getScoreBoard() {
+        let scoreBoard = [];
+        for (let i = 0; i < 10; i++) {
+            let fileName = localStorage.getItem('fileName' + i);
+            let score = localStorage.getItem('score' + i);
+            if (fileName != null && score != null) {
+                scoreBoard.push(fileName);
+                scoreBoard.push(score);
+            }
+        }
+        return scoreBoard;
     }
 }
